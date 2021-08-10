25e5bff301fbb5778916da2827c96a1b9baa6aca
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-08-10 19:35:18 +0530

    Added always online server

--- fileserver/file_server.py
+++ fileserver/file_server.py
@@ -1,5 +1,4 @@
 import socket
-import tqdm
 import os
 # device's IP address
 SERVER_HOST = "localhost"
@@ -21,36 +20,39 @@ s.bind((SERVER_HOST, SERVER_PORT))
 s.listen(5)
 print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
 
-# accept connection if there is any
-client_socket, address = s.accept() 
-# if below code is executed, that means the sender is connected
-print(f"[+] {address} is connected.")
+while True:
+    # accept connection if there is any
+    client_socket, address = s.accept() 
+    # if below code is executed, that means the sender is connected
+    print(f"[+] {address} is connected.")
 
-# receive the file infos
-# receive using client socket, not server socket
-received = client_socket.recv(BUFFER_SIZE).decode()
-filename, filesize = received.split(SEPARATOR)
-# remove absolute path if there is
-filename = os.path.basename(filename)
-# convert to integer
-filesize = int(filesize)
+    # receive the file infos
+    # receive using client socket, not server socket
+    received = client_socket.recv(BUFFER_SIZE).decode()
+    filename, filesize = received.split(SEPARATOR)
+    # remove absolute path if there is
+    filename = os.path.basename(filename)
+    # convert to integer
+    filesize = int(filesize)
 
-# start receiving the file from the socket
-# and writing to the file stream
-progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-with open(filename, "wb") as f:
-    while True:
-        # read 1024 bytes from the socket (receive)
-        bytes_read = client_socket.recv(BUFFER_SIZE)
-        if not bytes_read:    
-            # nothing is received
-            # file transmitting is done
-            break
-        # write to the file the bytes we just received
-        f.write(bytes_read)
-        # update the progress bar
-        progress.update(len(bytes_read))
+    # start receiving the file from the socket
+    # and writing to the file stream
 
+    with open(filename, "wb") as f:
+        while True:
+            # read 1024 bytes from the socket (receive)
+            bytes_read = client_socket.recv(BUFFER_SIZE)
+            if not bytes_read:    
+                # nothing is received
+                # file transmitting is done
+                break
+            # write to the file the bytes we just received
+            f.write(bytes_read)
+
+            # update the progress bar
+
+    print(f"Received {filename}")  
+    
 # close the client socket
 client_socket.close()
 # close the server socket
